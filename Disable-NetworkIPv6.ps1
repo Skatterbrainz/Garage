480ce89c9bf2d654553ec26f8379e6207cf6e207
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-03-18 11:11:59 -0400

    Create Disable-NetworkIPv6.ps1

---
+++ Disable-NetworkIPv6.ps1
@@ -0,0 +1,14 @@
+<#
+.DESCRIPTION
+    Disables IPv6 on all active network adapter bindings
+    
+.NOTES
+    Author: David M. Stein
+    Date: 03/18/2017
+#>
+
+Start-Transcript -Path "$env:TEMP\disable_ipv6.log"
+Get-NetAdapterBinding | 
+    ? {($_.ComponentId -eq 'ms_tcpip6') -and ($_.Enabled -eq $true)} | 
+        Disable-NetAdapterBinding -ComponentID 'ms_tcpip6'
+Stop-Transcript
