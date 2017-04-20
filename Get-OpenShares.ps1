5a9ae744b53dcf85bcda8608328b5793b8fc11cd
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-04-20 10:20:06 -0400

    Update Get-OpenShares.ps1

--- Get-OpenShares.ps1
+++ Get-OpenShares.ps1
@@ -1,4 +1,51 @@
-﻿$openshares = Get-WmiObject -Class Win32_Share -Filter "NOT name LIKE '%$'" -Property Path
-$openshares | foreach {$_.Path}
-$openshares.__PROPERTY_COUNT.count
 
+<#
+.SYNOPSIS
+    Returns not-hidden shares
+
+.DESCRIPTION
+    Returns the source paths of each share on one or more computers
+    where the share is not hidden (has a "$" suffix)
+
+.PARAMETER ComputerName
+    [optional] [string] one or more computer name to query.  If not
+    specified, the local computer is queried
+
+.NOTES
+    Author: David Stein
+    Date created: 11/02/2015
+    Date updated: 04/20/2017
+
+.EXAMPLE
+    Get-OpenShares -ComputerName fs1,fs2,fs3
+    
+#>
+
+function Get-OpenShares {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$False, HelpMessage="Name of computer")]
+        [string[]] $ComputerName
+    )
+    if (!($ComputerName)) { $ComputerName = "." }
+
+    foreach ($Computer in $ComputerName) {
+        try {
+            $openshares = Get-WmiObject -ComputerName $Computer -Class Win32_Share -Filter "NOT name LIKE '%$'" -Property Path -ErrorAction Stop
+            $props = @{
+                "ComputerName" = $Computer;
+                "Shares" = $openShares | Select-Object -ExpandProperty Path;
+                "Status" = 1;
+            }
+        }
+        catch {
+            $props = @{
+                "ComputerName" = $Computer;
+                "Shares" = $null;
+                "Status" = 0;
+            }
+        }
+        $obj = New-Object -TypeName PSObject -Property $props
+        Write-Output $obj
+    }
+}
