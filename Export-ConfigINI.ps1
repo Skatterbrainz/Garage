5a1f6f0113e72ef994d8b15a0d107f2f6dc46877
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-08-10 01:59:19 -0400

    Update Export-ConfigINI.ps1

--- Export-ConfigINI.ps1
+++ Export-ConfigINI.ps1
@@ -1,23 +1,21 @@
-function Export-ConfigINI {
-    param (
-        [parameter(Mandatory=$True)] $DataSet,
-        [parameter(Mandatory=$True)] [string] $Section,
-        [parameter(Mandatory=$True)] [string] $FilePath,
-        [parameter(Mandatory=$False)] [string] $Heading = "GENERAL",
-        [parameter(Mandatory=$False)] [switch] $Append
-    )
-    $result = 0
-    try {
-        if ($Append) {
-            "`[$Heading`]" | Out-File $FilePath -Append
-        }
-        else {
-            "`[$Heading`]" | Out-File $FilePath
-        }
-        $DataSet.$Section.Keys | ForEach-Object {$_ + '=' + $DataSet.$Section.$_} | Out-File $FilePath -Append
+param (
+    [parameter(Mandatory=$True)] $DataSet,
+    [parameter(Mandatory=$True)] [string] $Section,
+    [parameter(Mandatory=$True)] [string] $FilePath,
+    [parameter(Mandatory=$False)] [string] $Heading = "GENERAL",
+    [parameter(Mandatory=$False)] [switch] $Append
+)
+$result = 0
+try {
+    if ($Append) {
+        "`[$Heading`]" | Out-File $FilePath -Append
     }
-    catch {
-        $result = -1
+    else {
+        "`[$Heading`]" | Out-File $FilePath
     }
-    Write-Output $result
+    $DataSet.$Section.Keys | ForEach-Object {$_ + '=' + $DataSet.$Section.$_} | Out-File $FilePath -Append
 }
+catch {
+    $result = -1
+}
+Write-Output $result
