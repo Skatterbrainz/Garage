81aeed017be076e482c4e23d54df70e4d7db2eb3
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-08-11 12:27:35 -0400

    Update Run-BatchWinRS.ps1

--- Utilities/Run-BatchWinRS.ps1
+++ Utilities/Run-BatchWinRS.ps1
@@ -4,12 +4,16 @@ param (
         [string] $FilePath,
     [parameter(Mandatory=$True)]
         [ValidateNotNullOrEmpty()]
-        [string] $PayloadFile
+        [string] $PayloadFile,
+    [parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string] $RunasUser,
+    [parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string] $RunasPassword
 )
 $Servers  = Get-Content $FilePath
-$UserName = "AdminUser"
-$Password = 'P@ssW0rd123'
 
 foreach ($Server in $Servers) {
-    Write-Output "winrs -r:$Server -u:$UserName -p:$Password $PayloadFile"
+    Write-Output "winrs -r:$Server -u:$RunasUser -p:$RunasPassword $PayloadFile"
 }
