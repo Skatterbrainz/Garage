6ebb617db1d9d5fd6bc34865988bd8f0a2113785
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-08-11 12:58:51 -0400

    Update and rename Ping-IpRange.ps1 to Utilities/Ping-IpRange.ps1

--- Utilities/Ping-IpRange.ps1
+++ Utilities/Ping-IpRange.ps1
@@ -1,22 +1,20 @@
 param (
-	[parameter(Mandatory=$True,
-		HelpMessage="Base subnet without a trailing period")] 
+	[parameter(Mandatory=$True, HelpMessage="Base subnet without a trailing period")] 
 		[string] $SubnetBase = "192.168.0",
-	[parameter(Mandatory=$True,
-        HelpMessage="Enter range in parenthesis such as (2..10)")] $SubnetRange,
+	[parameter(Mandatory=$True, HelpMessage="Enter range in parenthesis such as (2..10)")] 
+		$SubnetRange,
     [parameter(Mandatory=$False)]
         [switch] $ResolveNames
 )
 
 Write-Host "pinging $SubnetBase.$($SubnetRange[0]) to $SubnetBase.$($SubnetRange[$SubnetRange.Length-1])" -ForegroundColor Green
-write-host
 
 $ping = New-Object System.Net.Networkinformation.Ping
 
 $result = @()
 
-Write-Host "IP Address`t`tHost Name`t`t`tResponse"
-Write-Host "-------------`t-----------------`t----------"
+Write-Output "IP Address`t`tHost Name`t`t`tResponse"
+Write-Output "-------------`t-----------------`t----------"
 
 foreach ($subx in $SubnetRange) {
     $addr = "$SubnetBase.$subx"
@@ -27,20 +25,20 @@ foreach ($subx in $SubnetRange) {
             $ErrorActionPreference = "SilentlyContinue"
             $dn = [System.Net.Dns]::GetHostEntry($addr)
             if ($dn) {
-                Write-Host "$addr`t$($dn.HostName)`t$($x.RoundtripTime)"
+                Write-Output "$addr`t$($dn.HostName)`t$($x.RoundtripTime)"
             }
             $ErrorActionPrefernce = $CurrentEAP
         }
         else {
-            Write-Host "$addr`t$($x.RoundtripTime)"
+            Write-Output "$addr`t$($x.RoundtripTime)"
         }
     }
     else {
         if ($ResolveNames) {
-            Write-Host "$addr`t----------------`toffline"
+            Write-Output "$addr`t----------------`toffline"
         }
         else {
-            Write-Host "$addr`toffline"
+            Write-Output "$addr`toffline"
         }
     }
 }
