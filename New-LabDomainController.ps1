13277565530e11d921483e4af62b0170b00ff3db
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-07-27 14:19:33 -0400

    added config data

--- New-LabDomainController.ps1
+++ New-LabDomainController.ps1
@@ -15,10 +15,11 @@ function New-LabDomainController {
     [parameter(Mandatory=$False, HelpMessage="Install DHCP role")]
       [switch] $InstallDHCP
   )
-
-  Write-Verbose "domain  = $DomainName"
-  Write-Verbose "dcname  = $DCName"
-  Write-Verbose "ipv4    = $IpAddress"
+  Start-Transcript
+  
+  Write-Verbose "domain... $DomainName"
+  Write-Verbose "dcname... $DCName"
+  Write-Verbose "ipv4..... $IpAddress"
 
   # if no gateway provided, default to .1 of guest vm address
 
@@ -26,7 +27,7 @@ function New-LabDomainController {
     Write-Verbose "configuring default IP gateway address"
     $IpGateway = $($IpAddress.split('.')[0..2] -join '.') + '.1'
   }
-  Write-Verbose "gateway = $IpGateway"
+  Write-Verbose "gateway.. $IpGateway"
 
   if ($env:COMPUTERNAME -ne $DCName) {
     Write-Verbose "renaming computer to $DCName"
@@ -43,24 +44,62 @@ function New-LabDomainController {
 
   Import-Module ServerManager
 
-  Write-Verbose "configuring static network IP address and dns server list"
+  if ((Get-NetIPAddress | Where-Object {$_.InterfaceAlias -eq 'Ethernet' -and $_.AddressFamily -eq 'IPv4'} | Select-Object -ExpandProperty IPAddress) -ne $IpAddress) {
+    Write-Verbose "configuring static network IP address and dns server list"
 
-  Get-NetIpAddress | 
-    Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.InterfaceAlias -eq 'Ethernet'} | 
-      New-NetIpAddress -IPAddress $IpAddress -PrefixLength 24 -DefaultGateway $IpGateway
-
-  Get-NetIpAddress | 
-    Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.InterfaceAlias -eq 'Ethernet'} | 
-      Set-DnsClientServerAddress -ServerAddresses $IpAddress
-
+    Get-NetIpAddress | 
+      Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.InterfaceAlias -eq 'Ethernet'} | 
+        New-NetIpAddress -IPAddress $IpAddress -PrefixLength 24 -DefaultGateway $IpGateway
+  
+    Get-NetIpAddress | 
+      Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.InterfaceAlias -eq 'Ethernet'} | 
+        Set-DnsClientServerAddress -ServerAddresses $IpAddress
+  }
+  else {
+    Write-Verbose "ip address is already set to $IpAddress"
+  }
+  
   Write-Verbose "installing AD domain services role"
-  Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+  Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools -Force -Confirm:$False
 
   Write-Verbose "creating AD forest and domain"
-  Install-ADDSForest -DomainName $DomainName -InstallDNS
+  Install-ADDSForest -DomainName $DomainName -InstallDNS -NoRebootOnCompletion
   
   if ($InstallDHCP) {
     Write-Verbose "installing DHCP role and features"
     Install-WindowsFeature DHCP -IncludeAllSubFeature -IncludeManagementTools
   }
+  
+  Write-Verbose "creating organizational unit structure"
+  
+  $LdapRoot = (Get-ADDomain).DistinguishedName
+  New-ADOrganizationalUnit -Name "Corp" -Path $LdapRoot
+  $NextRoot = "OU=Corp,$LdapRoot"
+  New-ADOrganizationalUnit -Name "Servers" -Path $NextRoot
+  New-ADOrganizationalUnit -Name "Workstations" -Path $NextRoot
+  New-ADOrganizationalUnit -Name "Users" -Path $NextRoot
+  New-ADOrganizationalUnit -Name "Groups" -Path $NextRoot
+  New-ADOrganizationalUnit -Name "ServiceAccounts" -Path $NextRoot
+  
+  Write-Verbose "creating user accounts"
+  $pwd = ConvertTo-SecureString -String "P@ssw0rd" -AsPlainText -Force
+
+  $UserRoot = "OU=Users,$NextRoot"
+  $normalusers = @("norfolk1=Norfolk User 1","norfolk2=Norfolk User 2","sccmadmin=SCCM Admin")
+  
+  foreach ($acct in $normalusers) {
+    $ulist = $acct.Split('=')
+    Write-Verbose "creating user: $($ulist[0])"
+    New-ADUser -Name $ulist[0] -AccountPassword $pwd -ChangePasswordAtLogon:$False -DisplayName $ulist[1] -Enabled:$True -Path $UserRoot
+  }
+  
+  $AcctRoot = "OU=ServiceAccounts,$NextRoot"
+  $svcaccounts = @("cm-client=CM Client Install","cm-domjoin=CM Domain Join","cm-naa=CM Network Access","cm-svcsql=CM SQL Service")
+  
+  foreach ($acct in $svcaccounts) {
+    $ulist = $acct.Split('=')
+    Write-Verbose "creating user: $($ulist[0])"
+    New-ADUser -Name $ulist[0] -AccountPassword $pwd -ChangePasswordAtLogon:$False -DisplayName $ulist[1] -Enabled:$True -Path $UserRoot
+  }
+  Write-Host "complete!" -ForegroundColor Green
 }
