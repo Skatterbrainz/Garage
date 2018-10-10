e38472315b8119106caa76e78f8a435f7f55fc0f
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-10-10 09:43:42 -0500

    Update Check-OneDriveVersion.ps1

--- Office/Check-OneDriveVersion.ps1
+++ Office/Check-OneDriveVersion.ps1
@@ -1,13 +1,80 @@
+#requires -Version 3
+<#
+.DESCRIPTION
+    Queries OneDrive sync client version for each user on each computer in a list
+.PARAMETER InputType
+    List: AD or File
+.PARAMETER InputFile
+    File with computer names
+.PARAMETER RequiredVersion
+    Version to compare and show "Good" or not
+.PARAMETER SelectComputers
+    Displays computer names in gridview to select individually
+.NOTES
+    1.0.0 - DS - First release
+    1.0.1 - DS - Added AD input option
+    1.0.2 - DS - Added SelectComputers option
+.EXAMPLE
+    .\Check-OneDriveVersion.ps1 -InputType AD -SelectComputers
+.EXAMPLE
+    .\Check-OneDriveVersion.ps1 -InputType File -InputFile ".\computers.txt" -SelectComputers
+#>
+
 [CmdletBinding()]
 param (
     [parameter(Mandatory = $False)]
-    [string] $InputFile = "$($env:USERPROFILE)\Documents\pilot_computers.txt",
+        [ValidateSet('AD','File')]
+        [string] $InputType = 'AD',
     [parameter(Mandatory = $False)]
-    [string] $RequiredVersion = '18.151.0729.0012',
+        [string] $InputFile = "$($env:USERPROFILE)\Documents\pilot_computers.txt",
     [parameter(Mandatory = $False)]
-    [switch] $ShowAll
+        [string] $RequiredVersion = '18.151.0729.0012',
+    [parameter(Mandatory = $False)]
+        [switch] $SelectComputers,
+    [parameter(Mandatory = $False)]
+        [switch] $ShowAll
 )
-$computers = Get-Content $InputFile
+switch ($InputType) {
+    'AD' {
+        Write-Host "retrieving computers from Active Directory..." -ForegroundColor Cyan
+        $strFilter = "(objectCategory=Computer)"
+        $objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+        $objSearcher.Filter = $strFilter
+        $objSearcher.PageSize = 2000
+        $objPath = $objSearcher.FindAll()
+        $computers = @()
+
+        foreach ($objItem in $objPath) {
+            try {
+                $objComputer = $objItem.GetDirectoryEntry()
+                $computers += ($objComputer.name).ToString()
+            }
+            catch {
+                # uh-oh, it's implosion time
+                Write-Error $_.Exception.Message
+            }
+        }
+        Write-Host "$($computers.count) computers found in AD" -ForegroundColor Cyan
+        break
+    }
+    'File' {
+        if (!(Test-Path -Path $InputFile)) {
+            Write-Warning "$InputFile not found!"
+            break
+        }
+        $computers = Get-Content $InputFile
+        Write-Host "$($computers.count) computers imported from file" -ForegroundColor Cyan
+        break
+    }
+} # switch
+
+if ($SelectComputers) {
+    $computers = $computers | Sort-Object | Out-GridView -Title "Select Computers" -OutputMode Multiple
+    Write-Host "$($computers.count) selected for processing"
+}
+else {
+    Write-Host "processing for $($computers.count) computers" -ForegroundColor Cyan
+}
 
 foreach ($computer in $computers) {
     if ((Test-NetConnection -ComputerName $computer -WarningAction SilentlyContinue).PingSucceeded) {
@@ -26,13 +93,11 @@ foreach ($computer in $computers) {
                 try {
                     $item = Get-ChildItem -Path "$opath" -ErrorAction SilentlyContinue
                     if ($item.VersionInfo.ProductVersion -eq $RequiredVersion) {
-                        if ($ShowAll) {
-                            $data = @{
-                                HostName = $computer
-                                UserName = $prof.Name
-                                OneDrive = $item.VersionInfo.ProductVersion
-                                Status   = "Good"
-                            }
+                        $data = @{
+                            HostName = $computer
+                            UserName = $prof.Name
+                            OneDrive = $item.VersionInfo.ProductVersion
+                            Status   = "Good"
                         }
                     }
                     else {
@@ -40,7 +105,7 @@ foreach ($computer in $computers) {
                             HostName = $computer
                             UserName = $prof.Name
                             OneDrive = $item.VersionInfo.ProductVersion
-                            Status   = "Outdated"
+                            Status   = $null
                         }
                         $result = New-Object PSobject -Property $data
                     }
@@ -57,15 +122,13 @@ foreach ($computer in $computers) {
             }
         }
         catch {
-            if ($ShowAll) {
-                $data = @{
-                    HostName = $computer
-                    UserName = $prof.Name
-                    OneDrive = "Inaccessible"
-                    Status   = $null
-                }
-                $result = New-Object PSobject -Property $data
+            $data = @{
+                HostName = $computer
+                UserName = $prof.Name
+                OneDrive = "Inaccessible"
+                Status   = $null
             }
+            $result = New-Object PSobject -Property $data
         }
     }
     else {
