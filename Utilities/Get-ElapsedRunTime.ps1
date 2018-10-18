bb864fa2ad7d94cb27c482403550e9d64eeddfd5
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-10-18 12:53:40 -0400

    Update Get-ElapsedRunTime.ps1

--- Utilities/Get-ElapsedRunTime.ps1
+++ Utilities/Get-ElapsedRunTime.ps1
@@ -1,11 +1,20 @@
 function Get-ElapsedRunTime {
     param (
         $StartTime,
-        [int] $ItemCount
+        [int] $ItemCount,
+        [switch] $EstimateCompletion
     )
     $elapsed  = New-TimeSpan -Start $StartTime -End $(Get-Date)
     $eminutes = $elapsed.TotalMinutes
     $eseconds = $elapsed.TotalSeconds
     $avgtime = [math]::Round($eseconds / $ItemCount, 2)
-    Write-Output "$([math]::Round($eminutes,1)) minutes ($avgtime seconds per item)"
+    if ($EstimateCompletion) {
+        $totalSeconds = [math]::Round($avgtime * $ItemCount,2)
+        $estFinish = Get-Date($StartTime).AddSeconds($totalSeconds)
+        $suffix = " (Estimated Finish: $estFinish)"
+    }
+    else {
+        $suffix = ""
+    }
+    Write-Output "$([math]::Round($eminutes,1)) minutes ($avgtime seconds per item)$suffix"
 }
