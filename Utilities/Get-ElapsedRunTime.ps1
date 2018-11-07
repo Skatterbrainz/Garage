3944f11a061dd30e5e7427d9fb22fe2914271b4e
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-11-07 12:59:16 -0500

    Fixed math bug
    The avg elapsed time was incorrectly calculated as a cumulative value, rather than a baseline mean avg.

--- Utilities/Get-ElapsedRunTime.ps1
+++ Utilities/Get-ElapsedRunTime.ps1
@@ -1,13 +1,20 @@
 function Get-ElapsedRunTime {
+    [CmdletBinding()]
     param (
         $StartTime,
+        [int] $CurrentItem,
         [int] $ItemCount,
         [switch] $EstimateCompletion
     )
     $elapsed  = New-TimeSpan -Start $StartTime -End $(Get-Date)
     $eminutes = $elapsed.TotalMinutes
     $eseconds = $elapsed.TotalSeconds
-    $avgtime = [math]::Round($eseconds / $ItemCount, 2)
+    $avgtime = [math]::Round($eseconds / $CurrentItem, 2)
+    Write-Verbose "    total items....: $ItemCount"
+    Write-Verbose "    current item...: $CurrentItem"
+    Write-Verbose "    elapsed minutes: $eminutes"
+    Write-Verbose "    elapsed seconds: $eseconds"
+    Write-Verbose "    average seconds: $avgtime"
     if ($EstimateCompletion) {
         $totalSeconds = [math]::Round($avgtime * $ItemCount,2)
         $estFinish = Get-Date($StartTime).AddSeconds($totalSeconds)
