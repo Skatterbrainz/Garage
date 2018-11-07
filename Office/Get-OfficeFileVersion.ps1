1bb2bd0fafe1155ea73b0fa20b7784693959b623
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-11-06 22:08:48 -0500

    Update Get-OfficeFileVersion.ps1

--- Office/Get-OfficeFileVersion.ps1
+++ Office/Get-OfficeFileVersion.ps1
@@ -1,16 +1,26 @@
+<#
+.DESCRIPTION
+    query file version from remote computers under either Office 2016 
+    installation path for 32-bit or 64-bit versions
+.PARAMETER ComputerName
+    Name of remote computer
+.PARAMETER FileName
+    Name of file to query (default = WINWORD.exe)
+#>
+
 [CmdletBinding()]
 param (
-    [parameter(Mandatory=$True)]
-        [ValidateNotNullOrEmpty()]
-        [string] $ComputerName,
-    [parameter(Mandatory=$False)]
-        [ValidateNotNullOrEmpty()]
-        [string] $FileName = "WINWORD.exe"
+    [parameter(Mandatory=$True, HelpMessage="Name of computer")]
+    [ValidateNotNullOrEmpty()]
+    [string] $ComputerName,
+    [parameter(Mandatory=$False, HelpMessage="Name of file")]
+    [ValidateNotNullOrEmpty()]
+    [string] $FileName = "WINWORD.exe"
 )
-
+Write-Verbose "----------------------------------------------"
+Write-Verbose "(Get-OfficeFileVersion - $ComputerName)"
 $wdpath1 = "\\$ComputerName\C`$\Program Files\Microsoft Office\root\Office16\$FileName"
 $wdpath2 = "\\$ComputerName\C`$\Program Files (x86)\Microsoft Office\Root\Office16\$FileName"
-
 if (Test-Path $wdpath1 -ErrorAction SilentlyContinue) {
     $plat = 64
     $f = Get-Item -Path $wdpath1 -ErrorAction SilentlyContinue
