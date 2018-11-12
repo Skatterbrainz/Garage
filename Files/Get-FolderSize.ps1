213c13bddd05657ca47f4197f2cc2a95c97d2f0f
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-11-12 17:09:04 -0500

    Delete Get-FolderSize.ps1

--- Files/Get-FolderSize.ps1
+++
@@ -1,9 +0,0 @@
-function Get-FolderSize {
-	param (
-		[parameter(Mandatory=$True, HelpMessage="Path and name of folder")]
-		[ValidateNotNullOrEmpty()]
-		[string] $FolderPath
-	)
-	$colItems = (Get-ChildItem "$FolderPath" -Recurse | Measure-Object -Property length -sum)
-	"{0:N2}" -f ($colItems.sum/1GB) + " GB"
-}
