d3b36ae0db61269494159e5c6689a7182149444a
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-11-11 23:46:27 -0500

    Delete Get-ADSiteLinks.ps1

--- AD/Get-ADSiteLinks.ps1
+++
@@ -1,6 +0,0 @@
-#requires -modules ActiveDirectory
-param ()
-$ADX = Get-ADObject -Filter 'objectClass -eq "siteLink"' -SearchBase (Get-ADRootDSE).ConfigurationNamingContext -Property Options, Cost, ReplInterval, SiteList, Schedule | 
-    Select-Object Name, @{Name="SiteCount";Expression={$_.SiteList.Count}}, Cost, ReplInterval, @{Name="Schedule";Expression={If($_.Schedule){If(($_.Schedule -Join " ").Contains("240")){"NonDefault"}Else{"24x7"}}Else{"24x7"}}}, Options
-$ADX | Format-Table
-#$ADX | Out-GridView -Title "Active Directory Site Links"
