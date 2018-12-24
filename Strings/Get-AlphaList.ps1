2bb4a8d69a9f44b0f3c4c8392688aec34a129cc7
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-23 21:04:14 -0500

    Update Get-AlphaList.ps1

--- Strings/Get-AlphaList.ps1
+++ Strings/Get-AlphaList.ps1
@@ -1,9 +1,1 @@
-function Get-AlphaList {
-    $alphalist = @()
-    $char1 = 65
-    $char2 = $char1+25
-    for ($i = $char1; $i -le $char2; $i++) {
-        $alphalist += [char]$i
-    }
-    $alphalist
-}
+(65..90) | %{[char]$_}
