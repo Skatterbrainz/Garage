d78642e531e1f5c71a116d42ebe0e333f12fce5b
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-08-08 00:56:43 -0400

    Update and rename Font-Install.ps1 to pkgs/Fonts/Install-Font.ps1

--- pkgs/Fonts/Install-Font.ps1
+++ pkgs/Fonts/Install-Font.ps1
@@ -18,24 +18,19 @@ $errorLabel = "Computer Unavailable".PadRight($padVal," ")
 $openType = "(Open Type)"
 $regPath  = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts"
 $objShell = New-Object -ComObject Shell.Application
-if (!(Test-Path $fontFolder))
-{
+if (!(Test-Path $fontFolder)) {
     Write-Warning "$fontFolder - Not Found"
 }
-else
-{
+else {
     $objFolder = $objShell.namespace($fontFolder)
-    foreach ($pcName in $pcNames)
-    {
-        Try {
+    foreach ($pcName in $pcNames) {
+        try {
             Write-Output "$pcLabel : $pcName"
             $null = Test-Connection $pcName -Count 1 -ErrorAction Stop
             $destination = "\\",$pcname,"\c$\Windows\Fonts" -join ""
-            foreach ($file in $objFolder.items())
-            {
+            foreach ($file in $objFolder.items()) {
                 $fileType = $($objFolder.getDetailsOf($file, 2))
-                if (($fileType -eq "OpenType font file") -or ($fileType -eq "TrueType font file"))
-                {
+                if (($fileType -eq "OpenType font file") -or ($fileType -eq "TrueType font file")) {
                     $fontName = $($objFolder.getDetailsOf($File, 21))
                     $regKeyName = $fontName,$openType -join " "
                     $regKeyValue = $file.Name
